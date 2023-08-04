41e4232b11186f9b3c774e3b88bb39eae7e2d262
jvaikun <jeevan.vaikun@gmail.com>
2023-08-04 10:20:20 +0700

    Menu system additions Created logic for choice menus Creature data structure for choice menus Added starfield particle effect to title screen Enabled global player stats to track choices Revised title menu navigation code

--- Menu/MenuTitle.gd
+++ Menu/MenuTitle.gd
@@ -1,17 +1,21 @@
 extends Control
 
 onready var fade_screen = $FadeScreen
-onready var menu_list = $Main/MenuList.get_children()
+onready var menu_list = $Main.get_children()
+onready var start_prompt = $StartPrompt
 
+onready var menu_data = {
+	"main":$Main.get_children(),
+	"load":$Load.get_children(),
+	"options":$Options.get_children(),
+}
+
+var menu_name = "main"
 var menu_index = 0 setget set_index
-var is_idle = true
 
 
 func set_index(val):
-	var min_val = 0
-	if !is_idle:
-		min_val = 1
-	menu_index = clamp(val, min_val, menu_list.size() - 1)
+	menu_index = clamp(val, 0, menu_list.size() - 1)
 	for i in menu_list.size():
 		if i == menu_index:
 			menu_list[i].modulate = Color(1, 0, 0)
@@ -20,36 +24,39 @@ func set_index(val):
 
 
 func _ready():
-	for i in menu_list.size():
-		menu_list[i].visible = (i == 0)
+	if OS.get_name() == "HTML5":
+		$Load/LoadFile.hide()
+		$Load/Password.show()
+	else:
+		$Load/LoadFile.show()
+		$Load/Password.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("game_up"):
-		if !is_idle or !$Load.visible or !$Options.visible:
+	if start_prompt.visible:
+		if Input.is_action_just_pressed("game_action"):
+			start_prompt.hide()
+			$Main.show()
+			set_index(0)
+	else:
+		if Input.is_action_just_pressed("game_up"):
 			self.menu_index -= 1
-	if Input.is_action_just_pressed("game_down"):
-		if !is_idle or !$Load.visible or !$Options.visible:
+		if Input.is_action_just_pressed("game_down"):
 			self.menu_index += 1
-	if Input.is_action_just_pressed("game_action"):
-		match menu_index:
-			0:
-				is_idle = false
-				for i in menu_list.size():
-					menu_list[i].visible = (i != 0)
-				set_index(1)
-			1:
-				fade_screen.go_to_scene("res://Menu/StageSelect/StageSelect.tscn")
-			2:
-				transition("load")
-			3:
-				transition("option")
-			4:
-				get_tree().quit()
-	if Input.is_action_just_pressed("game_jump"):
-		if menu_index in [2,3]:
-			transition("main")
+		if Input.is_action_just_pressed("game_action"):
+			match menu_index:
+				0:
+					fade_screen.go_to_scene("res://Menu/MenuChoice.tscn")
+				1:
+					transition("load")
+				2:
+					transition("options")
+				3:
+					get_tree().quit()
+		if Input.is_action_just_pressed("game_jump"):
+			if menu_name != "main":
+				transition("main")
 
 
 func set_transition(value):
@@ -63,6 +70,7 @@ func transition(type):
 	screenshot.create_from_image(img)
 	$Transition.texture = screenshot
 	$Transition.show()
+	menu_name = type
 	match type:
 		"main":
 			$Transition.material.set_shader_param("mask", load("res://Menu/Transition/mask_wipe_in.tres"))
@@ -73,12 +81,12 @@ func transition(type):
 			$Transition.material.set_shader_param("mask", load("res://Menu/Transition/mask_wipe_out.tres"))
 			$Main.hide()
 			$Load.show()
-		"option":
+		"options":
 			$Transition.material.set_shader_param("mask", load("res://Menu/Transition/mask_wipe_out.tres"))
 			$Main.hide()
 			$Options.show()
 	var trans_tween = get_tree().create_tween()
-	trans_tween.tween_method(self, "set_transition", 0.0, 1.0, 1.0)
+	trans_tween.tween_method(self, "set_transition", 0.0, 1.0, 0.5)
 	trans_tween.play()
 	yield(trans_tween, "finished")
 	$Transition.hide()
--- Splash/SplashScreen.gd
+++ Splash/SplashScreen.gd
@@ -1,8 +1,9 @@
 extends Control
 
-#Child nodes
 onready var fade_screen = $FadeScreen
+
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	if anim_name == "AutoPlay":
-		fade_screen.go_to_scene("res://Menu/MenuTitle.tscn") #"res://Levels/Levels/Grassland_1_2OP.tscn"
+		fade_screen.go_to_scene("res://Menu/MenuTitle.tscn")
+
